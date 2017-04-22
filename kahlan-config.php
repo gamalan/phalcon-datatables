6eff19d3ed666544e9be1d9364163e9f996f8efe
gamalan <gamalanpro@gmail.com>
2017-04-22 09:04:50 +0700

    fix kahlan dependencies, add case insensitive

--- kahlan-config.php
+++ kahlan-config.php
@@ -1,22 +1,24 @@
 <?php
-use filter\Filter;
-use kahlan\reporter\coverage\exporter\Coveralls;
+use Kahlan\Filter\Filter;
+use Kahlan\Reporter\Coverage\Exporter\Coveralls;
 
-$args = $this->args();
-$args->argument('coverage', 'default', 3);
+/** @var \Kahlan\Cli\Kahlan $this */
+/** @var \Kahlan\Cli\CommandLine $args */
+$args = $this->commandLine();
+$args->option('coverage', 'default', 3);
 
-Filter::register('phalcon.namespace', function($chain) {
-  $this->_autoloader->addPsr4('Spec\\Models\\', __DIR__ . '/spec/models/');
+Filter::register('phalcon.namespace', function ($chain) {
+    $this->_autoloader->addPsr4('Spec\\Models\\', __DIR__ . '/spec/models/');
 });
 
-Filter::register('phalcon.coverage-exporter', function($chain) {
+Filter::register('phalcon.coverage-exporter', function ($chain) {
     $reporter = $this->reporters()->get('coverage');
     if (!$reporter) return;
 
     Coveralls::write([
-        'collector'      => $reporter,
-        'file'           => 'coveralls.json',
-        'service_name'   => 'travis-ci',
+        'collector' => $reporter,
+        'file' => 'coveralls.json',
+        'service_name' => 'travis-ci',
         'service_job_id' => getenv('TRAVIS_JOB_ID') ?: null
     ]);
     return $chain->next();
@@ -25,10 +27,10 @@ Filter::register('phalcon.coverage-exporter', function($chain) {
 Filter::apply($this, 'namespaces', 'phalcon.namespace');
 Filter::apply($this, 'reporting', 'phalcon.coverage-exporter');
 
-$di = new \Phalcon\DI\FactoryDefault();
-$di->setShared('db', function() {
-  return new \Phalcon\Db\Adapter\Pdo\Sqlite([
-    'dbname' => __DIR__ . '/spec/db.sqlite',
-  ]);
+$di = new \Phalcon\Di\FactoryDefault();
+$di->setShared('db', function () {
+    return new \Phalcon\Db\Adapter\Pdo\Sqlite([
+        'dbname' => __DIR__ . '/spec/db.sqlite',
+    ]);
 });
-\Phalcon\DI::setDefault($di);
+\Phalcon\Di::setDefault($di);
--- spec/suite/ParamsParserSpec.php
+++ spec/suite/ParamsParserSpec.php
@@ -1,216 +1,216 @@
 <?php
 namespace Spec;
 
-describe("ParamsParser", function() {
+describe("ParamsParser", function () {
 
-  describe("Malformed", function() {
+    describe("Malformed", function () {
 
-    before(function() {
-      $this->parser = new \DataTables\ParamsParser(10);
-    });
+        beforeAll(function () {
+            $this->parser = new \DataTables\ParamsParser(10);
+        });
 
-    it("should instantiate when _GET and _POST is empty", function() {
+        it("should instantiate when _GET and _POST is empty", function () {
 
-      $this->parser = new \DataTables\ParamsParser(10);
-      expect($this->parser)->toBeA('object');
+            $this->parser = new \DataTables\ParamsParser(10);
+            expect($this->parser)->toBeA('object');
 
-      $params = $this->parser->getParams();
-      expect($params['draw'])->toBe(null);
-      expect($params['start'])->toBe(1);
-      expect($params['length'])->toBe(10);
-      expect($params['search'])->toBe([]);
-      expect($params['columns'])->toBe([]);
-      expect($params['order'])->toBe([]);
+            $params = $this->parser->getParams();
+            expect($params['draw'])->toBe(null);
+            expect($params['start'])->toBe(1);
+            expect($params['length'])->toBe(10);
+            expect($params['search'])->toBe([]);
+            expect($params['columns'])->toBe([]);
+            expect($params['order'])->toBe([]);
 
-    });
+        });
 
-    it("->getPage()", function() {
-      expect($this->parser->getPage())->toBe(1);
-    });
+        it("->getPage()", function () {
+            expect($this->parser->getPage())->toBe(1);
+        });
 
-    it("->getColumnsSearch()", function() {
-      expect($this->parser->getColumnsSearch())->toBe([]);
-    });
+        it("->getColumnsSearch()", function () {
+            expect($this->parser->getColumnsSearch())->toBe([]);
+        });
 
-    it("->getSearchableColumns()", function() {
-      expect($this->parser->getSearchableColumns())->toBe([]);
-    });
+        it("->getSearchableColumns()", function () {
+            expect($this->parser->getSearchableColumns())->toBe([]);
+        });
 
-    it("->getDraw()", function() {
-      expect($this->parser->getDraw())->toBe(null);
-    });
+        it("->getDraw()", function () {
+            expect($this->parser->getDraw())->toBe(null);
+        });
 
-    it("->getLimit()", function() {
-      expect($this->parser->getLimit())->toBe(10);
-    });
+        it("->getLimit()", function () {
+            expect($this->parser->getLimit())->toBe(10);
+        });
 
-    it("->getOffset()", function() {
-      expect($this->parser->getOffset())->toBe(1);
-    });
+        it("->getOffset()", function () {
+            expect($this->parser->getOffset())->toBe(1);
+        });
 
-    it("->getColumns()", function() {
-      expect($this->parser->getColumns())->toBe([]);
-    });
+        it("->getColumns()", function () {
+            expect($this->parser->getColumns())->toBe([]);
+        });
 
-    it("->getColumnById()", function() {
-      expect($this->parser->getColumnById(0))->toBe(null);
-    });
+        it("->getColumnById()", function () {
+            expect($this->parser->getColumnById(0))->toBe(null);
+        });
 
-    it("->getSearch()", function() {
-      expect($this->parser->getSearch())->toBe([]);
-    });
+        it("->getSearch()", function () {
+            expect($this->parser->getSearch())->toBe([]);
+        });
 
-    it("->getOrder()", function() {
-      expect($this->parser->getOrder())->toBe([]);
-    });
+        it("->getOrder()", function () {
+            expect($this->parser->getOrder())->toBe([]);
+        });
 
-    it("->getSearchValue()", function() {
-      expect($this->parser->getSearchValue())->toBe('');
-    });
-
-  });
-
-  describe("GET&POST", function() {
-
-    it("should use POST", function() {
-
-      $_GET['draw'] = 1;
-
-      $parser = new \DataTables\ParamsParser(10);
-      expect($parser)->toBeA('object');
-      expect($parser->getDraw())->toBe(1);
+        it("->getSearchValue()", function () {
+            expect($this->parser->getSearchValue())->toBe('');
+        });
 
     });
 
-    it("should user GET", function() {
+    describe("GET&POST", function () {
 
-      $_SERVER['REQUEST_METHOD'] = 'POST';
-      $_POST['draw'] = 2;
+        it("should use POST", function () {
 
-      $parser = new \DataTables\ParamsParser(10);
-      expect($parser)->toBeA('object');
-      expect($parser->getDraw())->toBe(2);
+            $_GET['draw'] = 1;
 
-    });
+            $parser = new \DataTables\ParamsParser(10);
+            expect($parser)->toBeA('object');
+            expect($parser->getDraw())->toBe(1);
 
-  });
+        });
 
-  describe("Pagination", function() {
+        it("should user GET", function () {
 
-    it('should correctly bind a pagination', function() {
+            $_SERVER['REQUEST_METHOD'] = 'POST';
+            $_POST['draw'] = 2;
 
-      $_GET['start'] = 200;
+            $parser = new \DataTables\ParamsParser(10);
+            expect($parser)->toBeA('object');
+            expect($parser->getDraw())->toBe(2);
 
-      $parser = new \DataTables\ParamsParser(10);
-      expect($parser->getPage())->toBe(21);
-
-    });
-
-    it('should correctly floor a page', function() {
-
-      $_GET['start'] = 195;
-
-      $parser = new \DataTables\ParamsParser(10);
-      expect($parser->getPage())->toBe(20);
+        });
 
     });
 
-  });
+    describe("Pagination", function () {
 
-  describe("Search", function() {
+        it('should correctly bind a pagination', function () {
 
-    beforeEach(function() {
+            $_GET['start'] = 200;
 
-      $_GET = [
-        'columns' => [
-          [
-            'data' => 'column1',
-            'searchable' => "true",
-            'search' => [
-              'value' => "column_search"
-            ]
-          ],
-          [
-            'data' => 'column2',
-            'searchable' => "false",
-          ]
-        ],
-        'search'  => [
-          'value' => 'global_search',
-        ]
-      ];
+            $parser = new \DataTables\ParamsParser(10);
+            expect($parser->getPage())->toBe(21);
+
+        });
+
+        it('should correctly floor a page', function () {
+
+            $_GET['start'] = 195;
+
+            $parser = new \DataTables\ParamsParser(10);
+            expect($parser->getPage())->toBe(20);
+
+        });
 
     });
 
-    it("should set global and column search", function() {
+    describe("Search", function () {
 
-      $parser = new \DataTables\ParamsParser(10);
-      expect($parser->getSearchableColumns())->toBe(['column1']);
-      expect(count($parser->getColumnsSearch()))->toBe(1);
-      expect($parser->getSearchValue())->toBe('global_search');
+        beforeEach(function () {
 
-    });
+            $_GET = [
+                'columns' => [
+                    [
+                        'data' => 'column1',
+                        'searchable' => "true",
+                        'search' => [
+                            'value' => "column_search"
+                        ]
+                    ],
+                    [
+                        'data' => 'column2',
+                        'searchable' => "false",
+                    ]
+                ],
+                'search' => [
+                    'value' => 'global_search',
+                ]
+            ];
 
-    it("should not fall on empty search in column", function() {
+        });
 
-      unset($_GET['columns'][0]['search']['value']);
-      $parser = new \DataTables\ParamsParser(10);
-      expect(count($parser->getColumnsSearch()))->toBe(0);
+        it("should set global and column search", function () {
 
-    });
+            $parser = new \DataTables\ParamsParser(10);
+            expect($parser->getSearchableColumns())->toBe(['column1']);
+            expect(count($parser->getColumnsSearch()))->toBe(1);
+            expect($parser->getSearchValue())->toBe('global_search');
 
-    it("should not fall on empty global search", function() {
+        });
 
-      unset($_GET['search']['value']);
-      $parser = new \DataTables\ParamsParser(10);
-      expect($parser->getSearchValue())->toBe('');
+        it("should not fall on empty search in column", function () {
 
-    });
+            unset($_GET['columns'][0]['search']['value']);
+            $parser = new \DataTables\ParamsParser(10);
+            expect(count($parser->getColumnsSearch()))->toBe(0);
 
-  });
+        });
 
-  describe("Order", function() {
+        it("should not fall on empty global search", function () {
 
-    beforeEach(function() {
+            unset($_GET['search']['value']);
+            $parser = new \DataTables\ParamsParser(10);
+            expect($parser->getSearchValue())->toBe('');
 
-      $_GET['order'] = [
-        [
-          'column' => "column1",
-          'dir'    => "desc"
-        ],
-        [
-          'column' => "column2",
-          'dir'    => "asc"
-        ]
-      ];
-
-    });
-
-    it("should correctly parse a two-way order", function() {
-
-      $parser = new \DataTables\ParamsParser(10);
-      expect($parser->getOrder())->toBe([
-        [
-          'column' => "column1",
-          'dir'    => "desc"
-        ],
-        [
-          'column' => "column2",
-          'dir'    => "asc"
-        ]
-      ]);
+        });
 
     });
 
-  });
+    describe("Order", function () {
+
+        beforeEach(function () {
+
+            $_GET['order'] = [
+                [
+                    'column' => "column1",
+                    'dir' => "desc"
+                ],
+                [
+                    'column' => "column2",
+                    'dir' => "asc"
+                ]
+            ];
+
+        });
+
+        it("should correctly parse a two-way order", function () {
+
+            $parser = new \DataTables\ParamsParser(10);
+            expect($parser->getOrder())->toBe([
+                [
+                    'column' => "column1",
+                    'dir' => "desc"
+                ],
+                [
+                    'column' => "column2",
+                    'dir' => "asc"
+                ]
+            ]);
+
+        });
+
+    });
 
 
-  afterEach(function() {
+    afterEach(function () {
 
-    unset($_GET);
-    unset($_POST);
-    unset($_SERVER);
+        unset($_GET);
+        unset($_POST);
+        unset($_SERVER);
 
-  });
+    });
 
 });
